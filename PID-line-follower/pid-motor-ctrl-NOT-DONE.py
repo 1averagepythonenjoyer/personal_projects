140f366b8b190de5e263786a6e6bfa7ea0dbad70
1averagepythonenjoyer <143657738+1averagepythonenjoyer@users.noreply.github.com>
2024-11-06 08:20:18 +0000

    Update pid-motor-ctrl-NOT-DONE.py

--- PID-line-follower/pid-motor-ctrl-NOT-DONE.py
+++ PID-line-follower/pid-motor-ctrl-NOT-DONE.py
@@ -4,7 +4,17 @@ if __name__ == '__main__':   #sets address, comms speed of serial comm.
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     ser.flush()
 
+
+    #might not work with uints? 
     while True:
-        if ser.in_waiting > 0:
-            line = ser.readline().decode('utf-8').rstrip() #decodes machine gibberish into understandable data  
-            print(line) #prints data
+        if ser.in_waiting > 0: #add global definitions
+            pos = ser.readline().decode('utf-8').rstrip() #decodes machine gibberish into understandable data  
+            err = ser.readline().decode('utf-8').rstrip()   #receives all of the values from arduino. Works by reading each line separately
+            pidval = ser.readline().decode('utf-8').rstrip()
+            
+            print("line's position is: ", pos)
+            print("error value is: ", err)
+            print("pidvalue is: ", pidval)
+    
+
+
