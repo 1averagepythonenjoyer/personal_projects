556b61e52b2e6824e28df00894323569edbf28fc
1averagepythonenjoyer <143657738+1averagepythonenjoyer@users.noreply.github.com>
2024-11-14 11:10:24 +0000

    Update and rename pidlinefollower.ino to main.ino

--- pidlinefollower/main.ino
+++ pidlinefollower/main.ino
@@ -1,5 +1,6 @@
-#include <QTRSensors.h>  //install library
 #include "thingProperties.h"
+#include <QTRSensors.h>  //install library
+
 #include <L298N.h>
 QTRSensors qtr; //makes it easier to type funcs 
 
@@ -9,24 +10,26 @@ QTRSensors qtr; //makes it easier to type funcs
 
 #define EN_B 5
 #define IN1_B 6
-#define IN2_b 7
+#define IN2_B 7
 
 float RMcorrection = 1.0;
 float LMcorrection = 0.95;
 float lspeed;
 float lspeedfinal;
 float rspeedfinal;
+float Kd;
+float Ki;
+float Kp;
 
 const uint8_t SensorCount = 5;  //declare number of sensors in the format the library likes
 uint16_t sensorValues[SensorCount];
 int threshold[SensorCount];
 
-float error;  //declare variables 
-float PIDvalue;  
+uint16_t position;
+float previousError;
 
-float Kp; 
-float Ki;
-float Kd;
+//declare variables 
+float PIDvalue;  
 
 float multiP = 0.0;  //scaling vals
 float multiI = 0.0;
@@ -46,14 +49,19 @@ void setup() {
 
   qtr.setTypeAnalog();
   qtr.setSensorPins((const uint8_t[]){ A1, A2, A3, A4, A5 }, SensorCount);
-
-  //calibration start
   pinMode(D12, OUTPUT);
+  pinMode(D13, OUTPUT);
+  
+  if ((ArduinoCloud.connected() == 0))  {
+    digitalWrite(D13, HIGH);
+  }else {
+      //calibration start
   digitalWrite(D12, HIGH);          //turn on led, connected to digital pin 12, to indicate we are calibrating
-  for (uint16_t i = 0; i < 400; i++)  //400 repeats, so a few minutes worth of calibration
+  for (uint16_t i = 0; i < 5; i++)  //400 repeats, so a few minutes worth of calibration
   {
     qtr.calibrate();  //must ensure that sensors are placed at height which will be used for the line follow sequence
-    delay(500);
+    delay(250);
+  }
   }
   digitalWrite(D12, LOW);
   delay(500);
@@ -61,8 +69,27 @@ void setup() {
   delay(500);
   digitalWrite(D12, LOW);
   //calibration end
-  Serial.begin(9600); //begins serial communication with RPi at 9600 baud
-  
+}
+
+void loop() {
+  ArduinoCloud.update();
+  mainrobot();
+}
+
+void mainrobot() {
+  position = qtr.readLineBlack(sensorValues);
+  float error = 2000 - position;
+  while (sensorValues[0]>=970 && sensorValues[1]>=970 && sensorValues[2]>=980 && sensorValues[3]>=970 && sensorValues[4]>=970){ 
+    if (previousError>0){       //Turn left if the line was to the left before it left the line
+      movemotor(-230,230);
+    }
+    else {
+      movemotor(230,-230); // Else turn right
+    }
+    position = qtr.readLineBlack(sensorValues);
+
+    float PID(float error);
+  }
 }
 
 void movemotor(int left, int right) {
@@ -71,15 +98,15 @@ void movemotor(int left, int right) {
     motorR.forward();
   }
   else {
-    motorR.setspeed(right * RMcorrection)
+    motorR.setSpeed(right * RMcorrection);
     motorR.backward();
   }
   if (left > 0){
-    motorL.setspeed(left * LMcorrection);
+    motorL.setSpeed(left * LMcorrection);
     motorL.forward();
   }
   else {
-    motorL.setspeed(left * LMcorrection);
+    motorL.setSpeed(left * LMcorrection);
     motorL.backward();
   }
 }
@@ -100,7 +127,7 @@ float PID(float error) {   //PID algorithm function.
   lspeedfinal = lspeed + PIDvalue;
   rspeedfinal = lspeed - PIDvalue;
 
-  if (lspeedfinal > 255){
+  if (lspeedfinal > 255){ //prevents overshoot
     lspeedfinal = 255;
   }  
   if (lspeedfinal < -255){
@@ -109,53 +136,25 @@ float PID(float error) {   //PID algorithm function.
   if (rspeedfinal > 255) {
     rspeedfinal = 255;
   }
-  if (rspeedfinal) {
+  if (rspeedfinal  < -255) {
+    rspeedfinal = -255;
+  }
   movemotor(lspeedfinal, rspeedfinal);
   return PIDvalue;
-  }
-}
-
-void mainrobot() {
-  position = qtr.readLineBlack(sensorValues);
-  float error = 2000 - position;
-  while (sensorValues[0]>=970 && sensorValues[1]>=970 && sensorValues[2]>=980 && sensorValues[3]>=970 && sensorValues[4]>=970){ 
-    if(previousError>0){       //Turn left if the line was to the left before it left the line
-      movemotor(-230,230);
-    }
-    else{
-      movemotor(230,-230); // Else turn right
-    }
-    position = qtr.readLineBlack(sensorValues);
-
-    float PID(float error);
-}
-
-void loop() {
-  ArduinoCloud.update();
-  mainrobot()
 }
 
 
 
 
-/*
-  Since Kp is READ_WRITE variable, onKpChange() is
-  executed every time a new value is received from IoT Cloud.
-*/
-void onKpChange()  {
-  // Add your code here to act upon Kp change
+void onKdadjChange()  {
+  Kd = Kdadj;
 }
-/*
-  Since Kd is READ_WRITE variable, onKdChange() is
-  executed every time a new value is received from IoT Cloud.
-*/
-void onKdChange()  {
-  // Add your code here to act upon Kd change
+
+void onKiadjChange()  {
+  Ki = Kiadj;
 }
-/*
-  Since Ki is READ_WRITE variable, onKiChange() is
-  executed every time a new value is received from IoT Cloud.
-*/
-void onKiChange()  {
-  // Add your code here to act upon Ki change
+
+
+void onKpadjChange()  {
+  Kp = Kpadj;
 }
